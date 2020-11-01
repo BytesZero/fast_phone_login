716d10137f9fb1b9380430063a1f569df8ed08e3
Zero <1300326388@qq.com>
2020-11-01 15:45:56 +0800

    1、增加 Android 和 iOS 代码

---
+++ ios/Classes/FastPhoneLoginPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FastPhoneLoginPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FastPhoneLoginPlugin.m
@@ -0,0 +1,20 @@
+#import "FastPhoneLoginPlugin.h"
+
+@implementation FastPhoneLoginPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"fast_phone_login"
+            binaryMessenger:[registrar messenger]];
+  FastPhoneLoginPlugin* instance = [[FastPhoneLoginPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
